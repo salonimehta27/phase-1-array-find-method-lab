dc637451e7e7af1de099d4e154dc5efaff6f874c
Saloni <salonimehta2127@gmail.com>
2021-05-05 22:20:30 -0400

    commit tree e1f25629b2bb54d2c8046a825f03128060e916cc

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ] 
+  function superbowlWin(arrayOfObjects)
+  {
+   const win=arrayOfObjects.find(element=>element.result==='W');
+   if(!!win)
+   {
+     return win.year;
+   }
+  }
 
+// superbowlWin(record);
+
+  //Another method for practice 
+  
+
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+// ] 
+// function superbowlWin(record)
+// {
+//   function winningyear(element)
+//   {
+//     return element.result==='W'     
+//   }
+//   const win =record.find(winningyear);
+//   if(!!win)
+//   {
+//     return win.year;
+//   }
+// }
+
+// function superbowlWin(record)
+  // {
+  //   for(const element of record)
+  //   { 
+  //      if(element.result==='W')
+  //        {
+  //            return (element.year);
+  //        }
+  //   }
+  // }
+  // record.find(superbowlWin);
